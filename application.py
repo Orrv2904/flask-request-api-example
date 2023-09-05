72fd4c335355fb51d57dd15eae2ad3975914253b
Orrv2904 <Omarrv2904@gmail.com>
2023-09-05 14:24:54 -0600

    :lipstick:

--- application.py
+++ application.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+api_key = os.environ.get("X_RapidAPI_Key")
+api_host = os.environ.get("X_RapidAPI_Host")
+
+if api_key is None or api_host is None:
+    raise Exception("Debes configurar las variables de entorno X_RapidAPI_Key y X_RapidAPI_Host.")
 
 @app.route('/')
 def index():
@@ -15,31 +20,22 @@ def index():
 def album():
     artista = request.form['artista']
 
-    # Realiza la solicitud a la API de Spotify
     url = "https://spotify23.p.rapidapi.com/search/"
     querystring = {"q": artista, "type": "multi", "offset": "0", "limit": "10", "numberOfTopResults": "5"}
     headers = {
-        "X-RapidAPI-Key": "dd03069afbmsh67292d8018889fep197c7djsn3f98e606e1a2",
-        "X-RapidAPI-Host": "spotify23.p.rapidapi.com"
-    }
+    "X-RapidAPI-Key": api_key,
+    "X-RapidAPI-Host": api_host
+    }   
     response = requests.get(url, headers=headers, params=querystring)
     data = response.json()
-
-    # Imprime la respuesta completa de la API en la consola para depurar
     print(json.dumps(data, indent=4))
-
-    # Extrae los nombres de los álbumes y las imágenes de portada
     albums = []
     for item in data.get('albums', {}).get('items', []):
         album_name = item.get('data', {}).get('name', '')
         cover_url = item.get('data', {}).get('coverArt', {}).get('sources', [{}])[0].get('url', '')
         albums.append({'name': album_name, 'cover_url': cover_url})
 
-    # Renderiza una plantilla HTML para mostrar los álbumes
     return render_template('albums.html', artist=artista, albums=albums)
-
-
-
 
 if __name__ == '__main__':
     app.run()
